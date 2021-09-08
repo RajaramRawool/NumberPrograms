4b4e9e0fe6df76bcc504051d8280392a59b617e7
Rajaram Rawool <rajaram903@gamil.com>
2021-09-08 13:41:02 +0530

    Sorting algorithms

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,6 @@
 package com.company;
 
-import java.util.Scanner;
+import java.util.Arrays;
 
 public class Main {
 
@@ -9,6 +9,19 @@ public class Main {
 //        numberPrograms.reverseNumber(123456789);
 //        numberPrograms.isAutoMorphic(625);
 //        numberPrograms.isPeterson(145);
+//        ---------------------------------------
+        SortingAlgorithms sortingAlgorithms = new SortingAlgorithms();
+        int[] array1 = {4, 1, 3, 45, 1};
+        int[] array2 = {10, 9, 8, 7, 6};
 
+        sortingAlgorithms.bubbleSortAscending(array1);
+        System.out.println(Arrays.toString(array1));
+        sortingAlgorithms.bubbleSortDescending(array2);
+        System.out.println(Arrays.toString(array2));
+
+        sortingAlgorithms.insertionSortAscending(array2);
+        System.out.println(Arrays.toString(array2));
+        sortingAlgorithms.insertionSortDescending(array2);
+        System.out.println(Arrays.toString(array2));
     }
 }
--- src/com/company/SortingAlgorithms.java
+++ src/com/company/SortingAlgorithms.java
@@ -1,6 +1,72 @@
 package com.company;
 
+import java.util.Arrays;
+
 public class SortingAlgorithms {
+
+    public void bubbleSortAscending(int[] numbers){
+        boolean isSorted = true ;
+        for(int i = 0 ; i < numbers.length ; i++){
+            for(int j = 1 ; j < numbers.length-i ; j++){
+                int temp = numbers[j] ;
+                if (numbers[j] < numbers[j-1]){
+                    numbers[j] = numbers[j-1];
+                    numbers[j-1] = temp;
+                    isSorted = false;
+                }
+                if (isSorted){
+                    return;
+                }
+            }
+        }
+    }
+
+    public void bubbleSortDescending(int[] numbers){
+        boolean isSorted = true ;
+        for (int i = 0 ; i < numbers.length ; i++ ){
+            for ( int j = 1 ; j < numbers.length-i ; j++){
+                int temp = numbers[j] ;
+                if (numbers[j] > numbers[j-1]){
+                    numbers[j] = numbers[j-1];
+                    numbers[j-1] = temp;
+                    isSorted = false ;
+                }
+            }
+                if (isSorted){
+                    return;
+                }
+        }
+    }
+
+    public void insertionSortAscending(int[] numbers){
+        for(int i = 1 ; i < numbers.length ; i++){
+            int temp = numbers[i];
+            int j = i - 1 ;
+            while( j >= 0 && numbers[j] > temp){
+                numbers[j+1] = numbers[j];
+                j--;
+            }
+            numbers[j+1]= temp ;
+        }
+
+    }
+
+    public void insertionSortDescending(int[] numbers){
+        for (int i = 1 ; i < numbers.length ; i++){
+            int temp = numbers[i];
+            int j = i - 1 ;
+            while ( j >= 0 && numbers[j] < temp){
+                numbers[j+1] = numbers[j];
+                j-- ;
+            }
+            numbers[j+1] = temp ;
+
+        }
+    }
+
+
+
+
 
 
 
