2d4096064708c22d787b72405414826d2bd88920
Rajaram Rawool <rajaram903@gamil.com>
2021-09-08 07:14:13 +0530

    Created Sorting Class

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,14 +1,14 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	    NumberPrograms numberPrograms = new NumberPrograms();
-        numberPrograms.reverseNumber(123456789);
-        numberPrograms.isAutoMorphic(625);
-        numberPrograms.isPeterson(145);
-
-
+//	    NumberPrograms numberPrograms = new NumberPrograms();
+//        numberPrograms.reverseNumber(123456789);
+//        numberPrograms.isAutoMorphic(625);
+//        numberPrograms.isPeterson(145);
 
     }
 }
